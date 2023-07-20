bcc5e052321c37612b27b90142a9ae43fa53b86e
e-alvarado <ealvarado570@gmail.com>
2023-07-20 13:25:17 -0500

    import data

--- predict/viz.py
+++ predict/viz.py
@@ -1,3 +1,7 @@
 import pandas as pd
 
-ms_2021 = pd.read_csv('csv/match_stats/match_stats_2021.csv')
+mstats_2021 = pd.read_csv(r"C:\Users\mexic\atp-world-tour-tennis-data\csv\3_match_stats\match_stats_2021.csv")
+
+#initial verification of import
+print(len(mstats_2021.index))
+print(mstats_2021.shape[0])
